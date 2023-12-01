3b8538f9c329a754c96f058767de45eda7ecffdf
aid777 <aidar.abilkair@icloud.com>
2023-12-01 17:11:27 +0600

    5.1 2nd dropdown fail

--- src/app/create-resume/page.js
+++ src/app/create-resume/page.js
@@ -9,9 +9,14 @@ import SelectDate from '@/components/SelectDate'
 
 export default function CreateResume() {
     const [cities, setCities] = useState([])
+    const [countries, setCountries] = useState([])
   useEffect(()=>{
     axios.get(`${END_POINT}/api/region/cities`).then( res =>{
       setCities(res.data)
+    })
+
+    axios.get(`${END_POINT}/api/region/countries`).then( res =>{
+      setCountries(res.data)
     })
   }, [])
 
@@ -32,8 +37,22 @@ export default function CreateResume() {
             <AutoCompliteSelect placeholder="" type="text" label="Город проживания" size="fieldset-md" items = {cities} onSelect={onSelect}/>
 
             <h3>Основная информация</h3>
+            < SelectDate size="fieldset-sm" label="Дата рождения"/>
+            <fieldset className={"fieldset fieldset-sm"}>
+                <label>Пол</label>
+                <div className='radio-group'>
+                  <div  className='radio'>
+                    <input type="radio" name="gender" id="g1"/>
+                    <label htmlFor="g1">Мужской</label>
+                  </div>
+                  <div className='radio' > 
+                    <input type="radio" name="gender" id="g2"/>
+                    <label htmlFor="g2">Женский</label>
+                  </div>
+                </div>
+            </fieldset>
 
-             
+            <AutoCompliteSelect placeholder="" type="text" label="Гражданство" size="fieldset-md" items = {countries} onSelect={onSelect}/>
 
         </div>
 
